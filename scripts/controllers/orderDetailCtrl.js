38d37a1566865c493f0f12552f65548d4f5b1f78
Maketroli <maketroli@gmail.com>
2015-05-26 16:13:49 -0600

    confirm dialog

--- scripts/controllers/orderDetailCtrl.js
+++ scripts/controllers/orderDetailCtrl.js
@@ -76,5 +76,20 @@ angular.module('RecopeApp.controllers')
       ServiceData.updating();
     }
 
-  });
+  })
+
+.directive('ngConfirmClick', [
+  function(){
+    return {
+      link: function (scope, element, attr) {
+        var msg = attr.ngConfirmClick || "Are you sure?";
+        var clickAction = attr.confirmedClick;
+        element.bind('click',function (event) {
+          if ( window.confirm(msg) ) {
+            scope.$eval(clickAction)
+          }
+        });
+      }
+    };
+  }])
 
